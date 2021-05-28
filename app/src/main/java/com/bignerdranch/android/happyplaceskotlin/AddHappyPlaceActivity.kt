2f309b8c90f5e0332851a818e5fc894f31aa1133
Djumabaevs <djumabaevb@gmail.com>
2021-05-28 14:47:19 +0600

    date picker dialog show

--- app/src/main/java/com/bignerdranch/android/happyplaceskotlin/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/bignerdranch/android/happyplaceskotlin/AddHappyPlaceActivity.kt
@@ -3,10 +3,11 @@ package com.bignerdranch.android.happyplaceskotlin
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.bignerdranch.android.happyplaceskotlin.databinding.ActivityAddHappyPlaceBinding
 import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var ab: ActivityAddHappyPlaceBinding
     private var cal = Calendar.getInstance()
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
@@ -27,6 +28,18 @@ class AddHappyPlaceActivity : AppCompatActivity() {
             cal.set(Calendar.YEAR, year)
             cal.set(Calendar.MONTH, month)
             cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            R.id.et_date -> {
+                DatePickerDialog(this@AddHappyPlaceActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
         }
     }
 }
