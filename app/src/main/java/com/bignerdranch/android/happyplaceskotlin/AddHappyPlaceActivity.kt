23aa3c803e61275a964b5451beefcea8bbb7096a
Djumabaevs <djumabaevb@gmail.com>
2021-05-28 13:20:55 +0600

    add support action bar in 2nd binding

--- app/src/main/java/com/bignerdranch/android/happyplaceskotlin/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/bignerdranch/android/happyplaceskotlin/AddHappyPlaceActivity.kt
@@ -2,10 +2,21 @@ package com.bignerdranch.android.happyplaceskotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.happyplaceskotlin.databinding.ActivityAddHappyPlaceBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+    private lateinit var ab: ActivityAddHappyPlaceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        ab = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        setContentView(ab.root)
+
+        setSupportActionBar(ab.toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        ab.toolbarAddPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
