9eff8a038b53ea63903f8974e84de132765031c4
Djumabaevs <djumabaevb@gmail.com>
2021-05-28 13:19:16 +0600

    add App Theme compat as parent

--- app/src/main/java/com/bignerdranch/android/happyplaceskotlin/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/happyplaceskotlin/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.bignerdranch.android.happyplaceskotlin
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.happyplaceskotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mb: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        mb = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mb.root)
+
+        mb.fabAddHappyPlace.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
