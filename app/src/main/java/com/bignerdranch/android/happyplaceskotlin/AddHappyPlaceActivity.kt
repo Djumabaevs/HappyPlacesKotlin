72516e18136119adb9129bdfa17a0db328b80687
Djumabaevs <djumabaevb@gmail.com>
2021-05-30 15:47:09 +0600

    Dexter class finish

--- app/src/main/java/com/bignerdranch/android/happyplaceskotlin/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/bignerdranch/android/happyplaceskotlin/AddHappyPlaceActivity.kt
@@ -2,13 +2,19 @@ package com.bignerdranch.android.happyplaceskotlin
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.happyplaceskotlin.databinding.ActivityAddHappyPlaceBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var ab: ActivityAddHappyPlaceBinding
@@ -40,23 +46,29 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when(v!!.id) {
             R.id.et_date -> {
-                DatePickerDialog(this@AddHappyPlaceActivity,
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
                     dateSetListener,
                     cal.get(Calendar.YEAR),
                     cal.get(Calendar.MONTH),
-                    cal.get(Calendar.DAY_OF_MONTH)).show()
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
             }
             R.id.iv_place_image -> {
                 val pictureDialog = AlertDialog.Builder(this)
                 pictureDialog.setTitle("Select Action")
-                val pictureDialogItems = arrayOf("Select photo from Gallery", "Capture photo from camera")
-                pictureDialog.setItems(pictureDialogItems) {
-                    dialog, which ->
-                        when(which) {
-                            0 -> choosePhotoFromGallery()
-                            1 -> Toast.makeText(this@AddHappyPlaceActivity, "Camera selction coming soon...",
-                        Toast.LENGTH_SHORT).show()
-                        }
+                val pictureDialogItems = arrayOf(
+                    "Select photo from Gallery",
+                    "Capture photo from camera"
+                )
+                pictureDialog.setItems(pictureDialogItems) { dialog, which ->
+                    when (which) {
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(
+                            this@AddHappyPlaceActivity, "Camera selction coming soon...",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
                 pictureDialog.show()
             }
@@ -64,7 +76,29 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun choosePhotoFromGallery() {
+        Dexter.withContext(this).withPermissions(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object : MultiplePermissionsListener {
 
+            override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+              if(report.areAllPermissionsGranted()) {
+                  Toast.makeText(this@AddHappyPlaceActivity, "Storage READ/WRITE permissions are granted.",
+                  Toast.LENGTH_SHORT).show()
+              }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: List<PermissionRequest?>?,
+                token: PermissionToken?) {
+                    showRationalDialogForPermissions()
+            }
+        }).check()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this).setMessage("" + "It looks like you have turned off permission required " +
+        "for this feature. " + "It can be enabled under the Application settings")
     }
 
     private fun updateDateInView() {
