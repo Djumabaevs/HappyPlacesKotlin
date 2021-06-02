e7f5c31e7452a065ed92c596a978b4b01471541d
Djumabaevs <djumabaevb@gmail.com>
2021-06-02 16:18:09 +0600

    companion objects

--- app/src/main/java/com/bignerdranch/android/happyplaceskotlin/database/DatabaseHandler.kt
+++ app/src/main/java/com/bignerdranch/android/happyplaceskotlin/database/DatabaseHandler.kt
@@ -6,5 +6,20 @@ import android.database.sqlite.SQLiteOpenHelper
 class DatabaseHandler (context: Context) :
     SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
+    companion object {
+        private const val DATABASE_VERSION = 1 // Database version
+        private const val DATABASE_NAME = "HappyPlacesDatabase" // Database name
+        private const val TABLE_HAPPY_PLACE = "HappyPlacesTable" // Table Name
+
+        //All the Columns names
+        private const val KEY_ID = "_id"
+        private const val KEY_TITLE = "title"
+        private const val KEY_IMAGE = "image"
+        private const val KEY_DESCRIPTION = "description"
+        private const val KEY_DATE = "date"
+        private const val KEY_LOCATION = "location"
+        private const val KEY_LATITUDE = "latitude"
+        private const val KEY_LONGITUDE = "longitude"
+    }
 
 }
