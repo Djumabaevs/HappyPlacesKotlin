9081c3beb75c8c9000c70da4c4dfb4a3b7841630
Djumabaevs <djumabaevb@gmail.com>
2021-06-03 12:30:07 +0600

    save info started

--- app/src/main/java/com/bignerdranch/android/happyplaceskotlin/activities/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/bignerdranch/android/happyplaceskotlin/activities/AddHappyPlaceActivity.kt
@@ -14,6 +14,7 @@ import android.provider.MediaStore
 import android.provider.Settings
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -95,7 +96,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when(v!!.id) {
+        when (v!!.id) {
             R.id.et_date -> {
                 DatePickerDialog(
                     this@AddHappyPlaceActivity,
@@ -122,7 +123,14 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
             }
             R.id.btn_save -> {
                 //TODO save datamodel to database
-
+                when {
+                    ab.etTitle.text.isNullOrEmpty() -> {
+                        Toast.makeText(this, "Please enter title", Toast.LENGTH_SHORT).show()
+                    }
+                    ab.etDescription.text.isNullOrEmpty() -> {
+                        Toast.makeText(this, "Please enter description", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
         }
     }
