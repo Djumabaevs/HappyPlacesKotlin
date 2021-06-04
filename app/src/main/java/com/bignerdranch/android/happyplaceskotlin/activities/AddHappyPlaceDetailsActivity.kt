7cabc4416902dd372cef68f30d79e8cfddeb6736
Djumabaevs <djumabaevb@gmail.com>
2021-06-04 23:52:23 +0600

    add binding to details activity

--- app/src/main/java/com/bignerdranch/android/happyplaceskotlin/activities/AddHappyPlaceDetailsActivity.kt
+++ app/src/main/java/com/bignerdranch/android/happyplaceskotlin/activities/AddHappyPlaceDetailsActivity.kt
@@ -2,11 +2,18 @@ package com.bignerdranch.android.happyplaceskotlin.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.bignerdranch.android.happyplaceskotlin.R
+import com.bignerdranch.android.happyplaceskotlin.databinding.ActivityAddHappyPlaceDetailsBinding
 
 class AddHappyPlaceDetailsActivity : AppCompatActivity() {
+    private lateinit var detailsBinding: ActivityAddHappyPlaceDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        detailsBinding = ActivityAddHappyPlaceDetailsBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place_details)
+        setContentView(detailsBinding.root)
+
+
     }
 }
