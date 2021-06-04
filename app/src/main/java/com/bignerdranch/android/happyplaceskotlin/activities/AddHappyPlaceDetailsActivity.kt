6eaa17b37f3e74e7343da3ba9331ccab5a474514
Djumabaevs <djumabaevb@gmail.com>
2021-06-04 23:46:07 +0600

    add new activity

---
+++ app/src/main/java/com/bignerdranch/android/happyplaceskotlin/activities/AddHappyPlaceDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.happyplaceskotlin.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bignerdranch.android.happyplaceskotlin.R
+
+class AddHappyPlaceDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_happy_place_details)
+    }
+}
