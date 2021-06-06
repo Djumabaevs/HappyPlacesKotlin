b93ca982d6957260c9bfed3ea486654df8526773
Djumabaevs <djumabaevb@gmail.com>
2021-06-06 16:23:50 +0600

    new activity and fragment created  bunch of codes

---
+++ app/src/main/java/com/bignerdranch/android/happyplaceskotlin/activities/MapActivity.kt
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.happyplaceskotlin.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bignerdranch.android.happyplaceskotlin.R
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+
+    }
+}
